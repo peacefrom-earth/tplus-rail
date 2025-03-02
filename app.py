47f145c847b55f3f2a844459741d5e53ae618f5c
peacefrom-earth <thewish.club@protonmail.com>
2025-03-01 23:25:32 -0500

    Moved Flask app to root for Rail

--- app.py
+++ app.py
@@ -57,4 +57,4 @@ def login():
     return jsonify({'message': 'Invalid credentials'}), 401
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5001)
+    app.run(host='0.0.0.0' , port=5003)
