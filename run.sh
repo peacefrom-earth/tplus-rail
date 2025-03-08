947fd95fc0068f393aa378a617d9267c8995aba3
peacefrom-earth <thewish.club@protonmail.com>
2025-03-08 17:51:27 -0500

    shifted run.sh bindings to acc all ivp6

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 source venv/bin/activate #Activate virtual environment
-FLASK_ENV=production gunicorn -w 4 -b 0.0.0.0:8081 chat:app &
-FLASK_ENV=production gunicorn -w 4 -b 0.0.0.0:8080 app:app & #Start Flask-SocketIO for real-time chat
+FLASK_ENV=production gunicorn -w 4 -b "[::]:8081" chat:app &
+FLASK_ENV=production gunicorn -w 4 -b "[::]:8080" app:app & #Start Flask-SocketIO for real-time chat
 echo "Project is running! Open index.html in a browser"
