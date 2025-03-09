fd18d283a19dc5d9f17a3bdf90f4de876a7684af
peacefrom-earth <thewish.club@protonmail.com>
2025-03-09 19:26:36 -0400

    again shifted run.sh

--- t+-infra/run.sh
+++ t+-infra/run.sh
@@ -1,4 +1,10 @@
+\#!/bin/bash
 #!/bin/bash
-source venv/bin/activate #Activate virtual environment
-FLASK_ENV=production gunicorn -w 4 -b "0.0.0.0:$PORT" app:app & #Start Flask-SocketIO for real-time chat
+echo "Activating virtual environment..."
+source venv/bin/activate
+echo "Starting Gunicorn on port $PORT..."
+FLASK_ENV=production gunicorn -w 4 -b "0.0.0.0:$PORT" app:app
+echo "Gunicorn process ended unexpectedly."
+sleep 1000
+
 echo "Project is running! Open index.html in a browser"
