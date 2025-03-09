69dd24d34706e8b0d30d2f48ee0f949805134b3f
peacefrom-earth <thewish.club@protonmail.com>
2025-03-09 00:58:47 -0500

    simplified gunicorn to a single binding

--- run.sh
+++ run.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 source venv/bin/activate #Activate virtual environment
-FLASK_ENV=production gunicorn -w 4 -b "[::]:8081" chat:app &
 FLASK_ENV=production gunicorn -w 4 -b "[::]:8080" --log-level debug app:app & #Start Flask-SocketIO for real-time chat
 echo "Project is running! Open index.html in a browser"
