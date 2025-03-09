27cadbc4c55086495179842ea2d7dc4f2afb06dc
peacefrom-earth <thewish.club@protonmail.com>
2025-03-09 06:50:02 -0400

    shhift in run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source venv/bin/activate #Activate virtual environment
-FLASK_ENV=production gunicorn -w 4 -b "[::]:8080" --log-level debug app:app & #Start Flask-SocketIO for real-time chat
+FLASK_ENV=production gunicorn -w 4 -b "0.0.0.0:8080" --log-level debug app:app & #Start Flask-SocketIO for real-time chat
 echo "Project is running! Open index.html in a browser"
